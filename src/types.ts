bd4a8f9a4a9d8459762ccc8f67c6899163322259
Joshua Strobl <me@joshuastrobl.com>
2023-08-22 19:12:18 +0300

    Implement third-party project schedules data and ICS file generator

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,33 @@
 import { ComponentType, SVGProps } from "react";
 
+export enum CalendarEventCategory {
+  DEBIAN = "Debian",
+  FEDORA = "Fedora",
+  GNOME = "Gnome",
+  SOLUS = "Solus",
+  UBUNTU = "Ubuntu",
+}
+
+export enum CalendarEventType {
+  ABI_FREEZE = "ABI Freeze",
+  ALPHA = "Alpha",
+  BETA_FREEZE = "Beta Freeze",
+  BETA_RELEASE = "Beta Release",
+  BRANCHING = "Branching",
+  BRANCH_FREEZE = "Branch Freeze",
+  CHANGE_CHECKPOINT_SELF_CONTAINED = "Change Checkpoint (Self-Contained)",
+  CHANGE_CHECKPOINT_TESTABLE = "Change Checkpoint (Testable)",
+  CODE_COMPLETE_DEADLINE = "Code Completion Deadline",
+  EOL = "End-of-life",
+  FEATURE_FREEZE = "Feature Freeze",
+  FINAL_FREEZE = "Final Freeze",
+  IMPORT_FREEZE = "Import Freeze",
+  RELEASE = "Release",
+  RELEASE_CANDIDATE = "Release Candidate",
+  START = "Start of Development",
+  TESTING_WEEK = "Testing Week",
+}
+
 export enum WebsiteType {
   GITHUB,
   MASTODON,
@@ -8,6 +36,24 @@ export enum WebsiteType {
   WEBSITE,
   YOUTUBE,
 }
+
+export type CalendarEvent = {
+  dates: string | [string, string];
+  release: string;
+  types: CalendarEventType[];
+  url: string;
+};
+
+export type CalendarEventStrictDate = {
+  category: CalendarEventCategory;
+  dates: [string, string];
+  name: string; // Internal event name used in generation
+} & Omit<CalendarEvent, "dates">;
+
+export type CalendarSchedule = {
+  category: CalendarEventCategory;
+  events: CalendarEvent[];
+};
 
 export type Person = {
   description: () => JSX.Element;
