1c635ee8e1c465b72cc7f7eac99c5c6def4f7678
Joshua Strobl <me@joshuastrobl.com>
2024-01-28 17:45:10 +0200

    Update for 10.9 release

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const LATEST_BUDGIE_RELEASE = {
-  date: "2023-10-18",
-  post: "https://buddiesofbudgie.org/blog/budgie-10-8-2-released",
-  version: "10.8.2",
+  date: "2024-01-28",
+  post: "https://buddiesofbudgie.org/blog/budgie-10-9-released",
+  version: "10.9",
 };
